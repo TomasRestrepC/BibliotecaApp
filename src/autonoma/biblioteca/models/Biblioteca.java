7fd13b020e3d8c11f06ffc7337c0712ac8ceafa9
Tomas <tomaschicarestrepo@gmail.com>
2025-03-14 21:27:10 -0500

    Se agrega la clase Biblioteca

---
+++ src/autonoma/biblioteca/models/Biblioteca.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.biblioteca.models;
+
+/**
+ *
+ * @author tomas
+ */
+
+import java.util.ArrayList;
+
+public class Biblioteca {
+    
+    private ArrayList libros; 
+
+    public Biblioteca(ArrayList libros) {
+        this.libros = libros;
+    }
+    
+    
+    
+    public String mostrarLibros(){
+        return libro;
+    }
+    
+    public boolean agregarLibro(Libro){
+        
+    
+    }
+    
+    public Libro buscarLibro(long){
+        
+    }
+    
+    public boolean actualizarLibro(long,Libro){
+        
+    }
+    
+    public boolean eliminarLibro(long){
+        
+    }
+    
+    public ArrayList<Libro>(){
+        
+    }
+}
